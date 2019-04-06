6a77e4f09dbc007421f9a8e9e3097a60f9b4a49d
gonzicz <gonzicz83@gmail.com>
2019-04-06 10:29:54 +0200

    first commit

---
+++ src/test/java/pl/sda/MongoHelloWordTest.java
@@ -0,0 +1,32 @@
+package pl.sda;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.assertj.core.api.Assertions;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MongoHelloWordTest {
+    @Test
+    @DisplayName("Show how to connect MongoDB using Java Driver")
+    void connectTest() {
+        MongoClient mongoClient = MongoClients.create(codecSettings());
+        MongoDatabase testDb = mongoClient.getDatabase("testDb");
+        MongoCollection<User> userCollection = testDb.getCollection("User", User.class);
+        User userToAdd = new User("Rafal", "Lewandowski");
+        userCollection.insertOne(userToAdd);
+        User firstUserInDb = userCollection.find().first();
+        assertThat(firstUserInDb.getFirstName()).isEqualTo("Rafal");
+        assertThat(firstUserInDb.getLastName()).isEqualTo("Lewandowski");
+    }
+    private MongoClientSettings codecSettings() {
+        return MongoClientSettings.builder().codecRegistry(CodecRegistries.fromRegistries(com.mongodb.MongoClient.getDefaultCodecRegistry(), CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()))).build();
+    }
+}
